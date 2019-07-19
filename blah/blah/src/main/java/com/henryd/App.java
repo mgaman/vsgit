e53e070a1a9717a676fdba5dcfc2625b6ec096ce
David Henry <mgadriver@gmail.com>
2019-07-19 22:12:20 +0300

    first pass

---
+++ blah/blah/src/main/java/com/henryd/App.java
@@ -0,0 +1,20 @@
+package com.henryd;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++) {
+            System.out.print("Hello World! ");
+            System.out.println(i);
+        }
+    }
+}
